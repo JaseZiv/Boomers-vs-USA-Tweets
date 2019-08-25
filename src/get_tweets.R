6150eebbd83846caf35b6f46da69e273bf96c36d
JaseZiv <jaseziv83@gmail.com>
2019-08-25 20:45:07 +1000

    finished twitter analysis

---
+++ src/get_tweets.R
@@ -0,0 +1,39 @@
+# load libraries
+library(rtweet) # for tweets
+library(ROAuth) # for twitter authorisation
+library(tidyverse) # data munging and visualisations
+library(lubridate) # working with dates
+library(scales) # plotting axis in a formatted way
+library(zoo) # working with mm-yyyy dates
+library(tidytext) # text analysis
+
+# The below code comes from the "rtweet" package. Much better as it doesn't shorten the tweets down to 140 characters
+# https://rtweet.info/articles/auth.html
+# https://rtweet.info/
+
+
+## authenticate via access token
+token <- create_token(
+  # app = "my_twitter_research_app",
+  consumer_key = Sys.getenv("twitter_consumer_key"),
+  consumer_secret = Sys.getenv("twitter_consumer_secret"),
+  access_token = Sys.getenv("twitter_access_token"),
+  access_secret = Sys.getenv("twitter_access_secret"))
+
+
+# Scrape tweets -----------------------------------------------------------
+
+boomers <- search_tweets("#BoomersUSA", n=18000, include_rts = FALSE)
+
+boomers2 <- search_tweets("@BasketballAus", n=18000, include_rts = FALSE)
+
+boomers_joined <- rbind(boomers, boomers2)
+
+
+boomers_joined <- boomers_joined %>% 
+  distinct(status_id, .keep_all = T) %>% 
+  distinct(text, .keep_all = T)
+
+
+saveRDS(boomers_joined, "data/boomers_usa_tweets.rds")
+
